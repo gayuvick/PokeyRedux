61761a0efc3f4cfeedb6fcd832b49bfb72987328
Gayu <gayuvick2002@gmail.com>
2024-06-13 15:26:18 +0530

    add accessibility linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,18 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "plugin:jsx-a11y/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+     "jsx-a11y/anchor-is-valid": "warn",
+    "jsx-a11y/no-redundant-roles": "warn"
   },
 }
