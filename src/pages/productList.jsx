6be2a004931aaab3e53968d87600a96ed49eecb9
Gayu <gayuvick2002@gmail.com>
2024-06-18 12:02:28 +0530

    check lint error

--- src/pages/productList.jsx
+++ src/pages/productList.jsx
@@ -3,7 +3,7 @@ import { useSelector , useDispatch} from 'react-redux';
 import { useEffect } from 'react';
 import pokeCard from "../assets/poke.jpg"
 
- 
+//  added comment
 export default function ProductList(){  
   
   const dispatch =  useDispatch();
